1d567ff5e38d8d5be6e541da373108c1853503b0
¨hildegw¨ <hildegw@snafu.de>
2017-11-15 20:48:47 -0500

    refactor adding data

--- components/deckList/AddDeckTitle.js
+++ components/deckList/AddDeckTitle.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
-import { Modal, View, Text, StyleSheet, Dimensions } from 'react-native'
+import { Modal, View, Text, TextInput, StyleSheet, Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import { getAllDecks, saveDeckTitle, addCardToDeck } from '../../utils/cardApi'
 import { grey, greyLight, yellowLight, white, orange } from '../../utils/colors'
 import { allDecks } from './deckListAction'
-import DeckInputForm from './DeckInputForm'
 import SelectButton from '../deck/SelectButton'
 
 class AddDeckTitle extends Component {
+
+  state = { value: ' ' }
 
   static navigationOptions = ({ navigation }) => {
     const { title } = navigation.state.params
     return { title: title}
   }
 
+  validate = (text) => {
+    return text.length < 3
+  }
+
 //TODO: combine onPressAddCard and onPressAddDeckTitle, just one button
-  onPressAddCard = (title) => {
+  onPressAddCard = () => {
     console.log('onPressAddCard, title-value', title)
     const question =
             {
@@ -28,8 +33,9 @@ class AddDeckTitle extends Component {
     //AsyncStorage mergeItem is not working on iOS, therefore handing over
     //original data set with all decks plus new questions to card API to setItem
     const { startData } = this.props
-    let result = addCardToDeck ({ question, title, startData })
-    // result = saveDeckTitle ({ title, startData })
+    const title = this.state.value
+    //let result = addCardToDeck ({ question, title, startData })
+    let result = saveDeckTitle ({ title, startData })
     //update startData state property
     getAllDecks().then((result) => {
         this.props.allDecks({startData: result})
@@ -37,32 +43,60 @@ class AddDeckTitle extends Component {
     this.props.navigation.goBack()
   }
 
-//TODO: ?? add global state for formIsEditable, set when clicking add deck or add button
-//TODO: add global state for deckSelected, to show in Form
+//TODO: set state with all three values and add to database
+//TODO: prepopulate fields if available
 
   render() {
     const deviceWidth = Dimensions.get('window').width
+    const inputError = this.validate(this.state.value)
 
     return (
       <View style={styles.container}>
-        <DeckInputForm
-          showButton={false}
-          children={'Add a new deck title'}
-          placeholder={'new deck title'} >
-        </DeckInputForm>
 
-        <DeckInputForm
-          showButton={false}
-          children={'Add a new question to the deck'}
-          placeholder={'new question'} >
-        </DeckInputForm>
+        <TextInput
+          style={[styles.textInput, {width: deviceWidth-40}]}
+          onChangeText={(value) => this.setState({value: value.trim()})}
+          autoFocus={true}
+          autoCapitalize={'words'}
+          maxLength={50}
+          placeholder={'new deck title'}
+          selectTextOnFocus={true}
+        />
 
-        <DeckInputForm
-          showButton={true}
-          onPress={this.onPressAddCard}
-          children={'Add a new answer to the deck'}
-          placeholder={'new answer'} >
-        </DeckInputForm>
+        <Text style={styles.text}> 'Add a new deck title'</Text>
+
+        <TextInput
+          style={[styles.textInput, {width: deviceWidth-40}]}
+          onChangeText={(value) => this.setState({value: value.trim()})}
+          autoFocus={true}
+          autoCapitalize={'words'}
+          maxLength={50}
+          placeholder={'new question'}
+          selectTextOnFocus={true}
+        />
+
+        <Text style={styles.text}> 'Add a new question to deck'</Text>
+
+        <TextInput
+          style={[styles.textInput, {width: deviceWidth-40}]}
+          onChangeText={(value) => this.setState({value: value.trim()})}
+          autoFocus={true}
+          autoCapitalize={'words'}
+          maxLength={50}
+          placeholder={'new answer'}
+          selectTextOnFocus={true}
+        />
+
+        <Text style={styles.text}> 'Add a new answer to question'</Text>
+
+        {!inputError &&
+          <SelectButton
+            onPress={() => this.onPressAddCard()}
+            children={'Add'}
+            style={[{borderColor: orange}, {width: 140}, {backgroundColor: 'transparent'}]} >
+          </SelectButton>
+        }
+
       </View>
 
   )}
@@ -76,6 +110,21 @@ const styles = StyleSheet.create({
     backgroundColor: yellowLight,
     padding: 20,
   },
+  textInput: {
+    margin: 5,
+    marginBottom: 10,
+    padding: 5,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  text: {
+    color: grey,
+    fontSize: 16,
+    textAlign: 'justify',
+    fontWeight: 'bold',
+    paddingLeft: 5,
+  }
 })
 
 function mapStateToProps (state) {
